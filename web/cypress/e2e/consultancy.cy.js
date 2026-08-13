0ff5f26518ea0ea21439bd678be1ec50996e6c91
Matheus <matheus@datamais.com.br>
2026-08-13 14:05:36 -0300

    Finalizando a Secao 4  do curso da udemy

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -1,5 +1,5 @@
 describe('Formulario de Consultoria', () => {
-    it('Deve solicitar consultoria individual', () => {
+    it.only('Deve solicitar consultoria individual', () => {
         cy.start()
         cy.submitLoginForm('papito@webdojo.com', 'katana123')
         cy.goTo('Formulários', 'Consultoria')
@@ -63,8 +63,12 @@ describe('Formulario de Consultoria', () => {
             .check()
         cy.contains('button', 'Enviar formulário')
             .click()
-        cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
+
+        cy.get('.modal', {timeout: 8000})
             .should('be.visible')
+            .find('.modal-content')
+            .should('be.visible')
+            .and('have.text', 'Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido.')
     })
 
     it('Deve verificar os campos obrigatorio', ()=>{
